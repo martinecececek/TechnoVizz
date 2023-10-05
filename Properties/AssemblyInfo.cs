1470ad374a84e73d9eb9fbed191d85c1479b8e55
martinecececek <104137335+martinecececek@users.noreply.github.com>
2023-10-05 20:26:21 +0200

    added new project

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("Technovizz")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Technovizz")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení atributu ComVisible na hodnotu False udělá typy v tomto sestavení neviditelné 
+// pro komponenty modelu COM.  Pokud potřebujete přistupovat k typům tohoto sestavení z 
+// modelu COM, nastavte atribut ComVisible daného typu na hodnotu True.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("21bb90d4-e135-4d4d-92c0-6c473e0ccc0d")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Dílčí verze 
+//      Číslo sestavení
+//      Revize
+//
+// Můžete zadat všechny hodnoty nebo nechat nastavená výchozí čísla sestavení a revize 
+// pomocí zástupného znaku * takto:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
