4a5eea22d470017b219dbcf8e06a1cede8c15ba0
martinecececek <104137335+martinecececek@users.noreply.github.com>
2023-10-05 20:44:02 +0200

    added object prarametrs

---
+++ Objekty/Material.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Technovizz.Objekty
+{
+    public class Material
+    {
+        public Material(string sAP, string nazev, string typPripravku)
+        {
+            SAP = sAP;
+            Nazev = nazev;
+            TypPripravku = typPripravku;
+        }
+
+        public String SAP { get; set; }
+        public String Nazev { get; set; }
+        public String TypPripravku { get; set; }
+
+    }
+}
--- Objekty/Project.cs
+++ Objekty/Project.cs
@@ -1,12 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Technovizz.Objekty
 {
-    internal class Project
+    public class Project
     {
+        public Project(string tl, string nazev, string popis, string sklo, string temp, string trh, string imds)
+        {
+            TL = tl;
+            Nazev = nazev;
+            ZkracenyPopis = popis;
+            Sklo = sklo;
+            Temp = temp;
+            Trh = trh;
+            Temp = temp;
+            IMDS = imds;
+        }
+
+        public String TL { get; set; }
+        public String Nazev { get; set; }
+        public String ZkracenyPopis { get; set; }
+        public String Sklo { get; set; }
+        public String Temp { get; set; }
+        public String Trh { get; set; }
+        public String IMDS { get; set; }
+
+
+
     }
 }
